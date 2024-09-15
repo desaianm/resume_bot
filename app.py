5d04a3bcc4f0a3c95d5fe93078e8e3baa2126f81
desainanm <desaianm@sheridancollege.ca>
2024-09-15 18:10:18 -0400

    modified:   app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ from langchain.chains import LLMChain
 from langchain_community.chat_models import ChatOllama
 import streamlit as st
 import PyPDF2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 
@@ -69,7 +72,7 @@ def retrieve_info(query):
     return page_contents_array
 
 # 3. Setup llm chain and prompts
-llm = ChatOpenAI(model_name="gpt-4o-mini",temperature=0.3)
+llm = ChatOpenAI(model_name="gpt-4o",temperature=1)
 
 
 # 4. Retreival Augmented Generation
